a6eda0aa1c18f2ff80597e91772432114b5db938
Jhobart <jhobart.poblete@traxtech.com>
2019-08-08 18:10:28 +0800

    Added Import App

---
+++ ImportExcelToDatabase/ImportExcelToDatabase/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ImportExcelToDatabase
+{
+    partial class Import
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Import));
+            this.selectFile_button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.import_button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectFile_button1
+            // 
+            this.selectFile_button1.Location = new System.Drawing.Point(12, 12);
+            this.selectFile_button1.Name = "selectFile_button1";
+            this.selectFile_button1.Size = new System.Drawing.Size(108, 29);
+            this.selectFile_button1.TabIndex = 0;
+            this.selectFile_button1.Text = "Select File";
+            this.selectFile_button1.UseVisualStyleBackColor = true;
+            this.selectFile_button1.Click += new System.EventHandler(this.selectFile_button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(679, 490);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(276, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(43, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Yes";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(325, 13);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "No";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Header?";
+            // 
+            // import_button2
+            // 
+            this.import_button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.import_button2.Location = new System.Drawing.Point(583, 543);
+            this.import_button2.Name = "import_button2";
+            this.import_button2.Size = new System.Drawing.Size(108, 29);
+            this.import_button2.TabIndex = 5;
+            this.import_button2.Text = "Import";
+            this.import_button2.UseVisualStyleBackColor = true;
+            this.import_button2.Click += new System.EventHandler(this.import_button2_Click);
+            // 
+            // Import
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 584);
+            this.Controls.Add(this.import_button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.selectFile_button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Import";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Import";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFile_button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button import_button2;
+    }
+}
+
---
+++ ImportExcelToDatabase/ImportExcelToDatabase/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace ImportExcelToDatabase
+{
+    public partial class Import : Form
+    {
+        public Import()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+        }
+
+        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+
+        private void selectFile_button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string filePath = openFileDialog1.FileName;
+            string extension = Path.GetExtension(filePath);
+            string header = radioButton1.Checked ? "YES" : "NO";
+            string conStr, sheetName;
+
+            conStr = string.Empty;
+            switch (extension)
+            {
+
+                case ".xls": //Excel 97-03
+                    conStr = string.Format(Excel03ConString, filePath, header);
+                    break;
+
+                case ".xlsx": //Excel 07
+                    conStr = string.Format(Excel07ConString, filePath, header);
+                    break;
+            }
+
+            //Get the name of the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString(); // Select which tab will be looked up.
+                    con.Close();
+                }
+            }
+
+            //Read Data from the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        DataTable dt = new DataTable();
+                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+
+                        //Populate DataGridView.
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+        }
+
+        private void import_button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new System.Data.SqlClient.SqlConnection();
+                con = new System.Data.SqlClient.SqlConnection();
+                con.ConnectionString = "Data Source=AppleAsiaProdSQL.traxtech.com;Initial Catalog=Apasia_Prod_02M12;Trusted_Connection=True";
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+
+                for (int i = 0; i <= dataGridView1.Rows.Count - 2; i++)
+                {
+
+                    String insertData = "INSERT INTO dbo.AppleAPACInvoicesToBeClosedPaid(INV_ID, INV_KEY, CHECK_NUM, PAID_AMOUNT, PAID_DATE) " +
+                                        "VALUES (@traxinvid, @invkey, @paymentnumber, @paidamount, @paymentdate)";
+                    SqlCommand cmd = new SqlCommand(insertData, con);
+                    //cmd.Parameters.AddWithValue("@scac", dataGridView1.Rows[i].Cells[0].Value);
+                    //cmd.Parameters.AddWithValue("@scac", dataGridView1.Rows[i].Cells[0].Value);
+                    cmd.Parameters.AddWithValue("@traxinvid", dataGridView1.Rows[i].Cells[0].Value);
+                    cmd.Parameters.AddWithValue("@invkey", dataGridView1.Rows[i].Cells[1].Value);
+                    cmd.Parameters.AddWithValue("@paymentnumber", dataGridView1.Rows[i].Cells[2].Value);
+                    cmd.Parameters.AddWithValue("@paidamount", dataGridView1.Rows[i].Cells[3].Value);
+                    //cmd.Parameters.AddWithValue("@paymentcurrency", dataGridView1.Rows[i].Cells[6].Value);
+                    cmd.Parameters.AddWithValue("@paymentdate", dataGridView1.Rows[i].Cells[4].Value);
+                    //cmd.Parameters.AddWithValue("@submittedby", dataGridView1.Rows[i].Cells[7].Value);
+                    //cmd.Parameters.AddWithValue("@comment", dataGridView1.Rows[i].Cells[8].Value);
+                    da.InsertCommand = cmd;
+                    cmd.ExecuteNonQuery();
+                }
+                con.Close();
+
+                string msg = "Records have been imported.";
+                string caption = "";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBoxIcon icon = MessageBoxIcon.Information;
+                MessageBox.Show(msg, caption, buttons, icon);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
